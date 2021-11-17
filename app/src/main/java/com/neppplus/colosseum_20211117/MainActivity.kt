e27d2f5ad6200ebfc7121422983be1b55b330b81
김준기 <gugi329@daum.net>
2021-11-17 15:14:13 +0900

    메인 액티비티 - BaseActivity 상속으로 변경

--- app/src/main/java/com/neppplus/colosseum_20211117/MainActivity.kt
+++ app/src/main/java/com/neppplus/colosseum_20211117/MainActivity.kt
@@ -5,13 +5,24 @@ import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.neppplus.colosseum_20211117.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
 
     lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
 
     }
+
+    override fun setValues() {
+
+    }
+
+
 }
