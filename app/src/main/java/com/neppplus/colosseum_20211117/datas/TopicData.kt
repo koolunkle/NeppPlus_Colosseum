d5c723fdbc68be3a73f6ed67b64782f1a68170dd
김준기 <gugi329@daum.net>
2021-11-18 15:18:13 +0900

    서버가 내려주는 토론 주제 데이터를 담기위한, 데이터 표현용 클래스 추가   - 임시 : id, 제목, 이미지 주소만 담자

---
+++ app/src/main/java/com/neppplus/colosseum_20211117/datas/TopicData.kt
@@ -0,0 +1,14 @@
+package com.neppplus.colosseum_20211117.datas
+
+class TopicData {
+
+//    생성자 : 기본 생성자 유지
+//    멤버변수만 따로 추가 -> JSON 파싱해서 변수에 채워넣자
+//    멤버변수 : 서버의 데이터를 보고 -> 담아주는 용도의 변수들로 만들자
+
+    var id = 0 // Int가 들어올 자리라는 표식
+    var title = "" // String이 들어올 자리
+    var imageURL = ""
+
+
+}
