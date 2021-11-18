4231448c7a5d302df0099a1e74433b4eb2db5475
김준기 <gugi329@daum.net>
2021-11-18 10:22:23 +0900

    서버 응답 분석  - code : 200이 아니면  - 왜 실패인지 message 에 담긴 String 토스트 출력

--- app/src/main/java/com/neppplus/colosseum_20211117/SignUpActivity.kt
+++ app/src/main/java/com/neppplus/colosseum_20211117/SignUpActivity.kt
@@ -2,8 +2,11 @@ package com.neppplus.colosseum_20211117
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.neppplus.colosseum_20211117.databinding.ActivityMainBinding
+import com.neppplus.colosseum_20211117.utils.ServerUtil
+import org.json.JSONObject
 
 class SignUpActivity : BaseActivity() {
 
@@ -31,11 +34,37 @@ class SignUpActivity : BaseActivity() {
 
 //            서버의 회원가입 기능에 전달 (Request) -> 돌아온 응답 대응 (Response)
 
+            ServerUtil.putRequestSignUp(
+                inputEmail,
+                inputPw,
+                inputNickname,
+                object : ServerUtil.JsonResponseHandler {
+                    override fun onResponse(jsonObj: JSONObject) {
+
+                        val code = jsonObj.getInt("code")
+
+                        if (code == 200) {
+
+                        } else {
+
+//                        실패 -> 서버가 알려주는 "message" 에 담긴 실패 사유를 토스트로
+                            val message = jsonObj.getString("message")
+
+                            runOnUiThread {
+                                Toast.makeText(mContext, message, Toast.LENGTH_SHORT).show()
+                            }
+
+                        }
+
+                    }
+                })
+
 
         }
 
 
     }
+
 
     override fun setValues() {
 
