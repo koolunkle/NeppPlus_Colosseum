a158f243bbcfc3d3a6f786527b1d0ba4813a65b1
김준기 <gugi329@daum.net>
2021-11-18 16:10:45 +0900

    실제 리스트 데이터 반영   - Glide 라이브러리 활용

--- app/src/main/java/com/neppplus/colosseum_20211117/adapters/TopicAdapter.kt
+++ app/src/main/java/com/neppplus/colosseum_20211117/adapters/TopicAdapter.kt
@@ -5,6 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import com.bumptech.glide.Glide
 import com.neppplus.colosseum_20211117.R
 import com.neppplus.colosseum_20211117.datas.TopicData
 
@@ -27,6 +30,15 @@ class TopicAdapter(
         }
 
         val row = tempRow!!
+
+        val data = mList[position]
+
+        val imgTopic = row.findViewById<ImageView>(R.id.imgTopic)
+        val txtTopicTitle = row.findViewById<TextView>(R.id.txtTopicTitle)
+
+        txtTopicTitle.text = data.title
+
+        Glide.with(mContext).load(data.imageURL).into(imgTopic)
 
         return row
 
