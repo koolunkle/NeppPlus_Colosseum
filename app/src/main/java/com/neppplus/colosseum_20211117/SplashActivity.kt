dff3b316d4efecf7de1972dbb92ab68b32f905ea
김준기 <gugi329@daum.net>
2021-11-19 12:47:26 +0900

    자동 로그인 기능 구현   - 토큰값 저장 여부에 따라, 어느 화면으로 갈지 다르게 설정

--- app/src/main/java/com/neppplus/colosseum_20211117/SplashActivity.kt
+++ app/src/main/java/com/neppplus/colosseum_20211117/SplashActivity.kt
@@ -1,9 +1,13 @@
 package com.neppplus.colosseum_20211117
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.databinding.DataBindingUtil
 import com.neppplus.colosseum_20211117.databinding.ActivitySplashBinding
+import com.neppplus.colosseum_20211117.utils.ContextUtil
 
 class SplashActivity : BaseActivity() {
 
@@ -21,6 +25,29 @@ class SplashActivity : BaseActivity() {
     }
 
     override fun setValues() {
+
+//         2.5초가 지나면 어느 화면으로 가는지 검사
+
+        val myHandler = Handler(Looper.getMainLooper())
+
+        myHandler.postDelayed({
+
+            val myIntent: Intent
+
+//          자동 로그인 할 상황? -> 메인화면
+//          아니라면? -> 로그인화면
+
+//            질문? 자동로그인 사용? + 로그인 성공?
+            if (ContextUtil.getAutoLogin(mContext) && ContextUtil.getToken(mContext) != "") {
+                myIntent = Intent(mContext, MainActivity::class.java)
+            } else {
+                myIntent = Intent(mContext, LoginActivity::class.java)
+            }
+
+            startActivity(myIntent)
+
+        }, 1000)
+
 
     }
 
