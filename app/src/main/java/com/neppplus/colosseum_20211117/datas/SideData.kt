2d3d607295442fb01377ebcfebf9463fb6b15bbf
김준기 <gugi329@daum.net>
2021-11-19 10:07:13 +0900

    선택 진영 데이터 표현용 클래스   - JSON > SideData 변환 함수 추가

---
+++ app/src/main/java/com/neppplus/colosseum_20211117/datas/SideData.kt
@@ -0,0 +1,28 @@
+package com.neppplus.colosseum_20211117.datas
+
+import org.json.JSONObject
+
+class SideData {
+
+    var id = 0
+    var title = ""
+    var voteCount = 0
+
+    companion object {
+
+        fun getSideDataFromJson(jsonObject: JSONObject): SideData {
+
+            val sideData = SideData()
+
+//            기본값 -> jsonObj를 이용해서 파싱된 값으로 대입
+            sideData.id = jsonObject.getInt("id")
+            sideData.title = jsonObject.getString("title")
+            sideData.voteCount = jsonObject.getInt("vote_count")
+
+            return sideData
+
+        }
+
+    }
+
+}
