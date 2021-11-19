b9c5979d3afde03c40f26fab4e38bc72853dfc54
김준기 <gugi329@daum.net>
2021-11-19 11:47:32 +0900

    댓글 추가 파싱 중 - 사용자 데이터 발견   - 데이터 클래스 추가 + 파싱 함수 추가

---
+++ app/src/main/java/com/neppplus/colosseum_20211117/datas/UserData.kt
@@ -0,0 +1,28 @@
+package com.neppplus.colosseum_20211117.datas
+
+import org.json.JSONObject
+
+class UserData {
+
+    var id = 0
+    var email = ""
+    var nickname = ""
+
+    companion object {
+
+        fun getUserDataFromServer(jsonObject: JSONObject): UserData {
+
+            val userData = UserData()
+
+            userData.id = jsonObject.getInt("id")
+            userData.email = jsonObject.getString("email")
+            userData.nickname = jsonObject.getString("nick_name")
+
+            return UserData()
+
+        }
+
+    }
+
+
+}
