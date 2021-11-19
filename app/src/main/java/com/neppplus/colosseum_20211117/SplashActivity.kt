a3d8de4299e132fc2ffd146dc9e2c5edfbd35b05
김준기 <gugi329@daum.net>
2021-11-19 12:38:27 +0900

    시작화면 (SplashActivity) 임시 추가

---
+++ app/src/main/java/com/neppplus/colosseum_20211117/SplashActivity.kt
@@ -0,0 +1,28 @@
+package com.neppplus.colosseum_20211117
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.neppplus.colosseum_20211117.databinding.ActivitySplashBinding
+
+class SplashActivity : BaseActivity() {
+
+    lateinit var binding: ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_splash)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+
+
+}
