d25c8abbaf649849b00ac493f20072945111fabc
김준기 <gugi329@daum.net>
2021-11-22 12:12:39 +0900

    대댓글 목록 표시   - Bug : 좋아요 / 싫어요 앱 강제종료

--- app/src/main/java/com/neppplus/colosseum_20211117/ViewReplyDetailActivity.kt
+++ app/src/main/java/com/neppplus/colosseum_20211117/ViewReplyDetailActivity.kt
@@ -3,6 +3,7 @@ package com.neppplus.colosseum_20211117
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import com.neppplus.colosseum_20211117.adapters.ReReplyAdapter
 import com.neppplus.colosseum_20211117.databinding.ActivityViewReplyDetailBinding
 import com.neppplus.colosseum_20211117.datas.ReplyData
 import com.neppplus.colosseum_20211117.utils.ServerUtil
@@ -15,6 +16,7 @@ class ViewReplyDetailActivity : BaseActivity() {
     lateinit var mReplyData: ReplyData
 
     val mReReplyList = ArrayList<ReplyData>()
+    lateinit var mReReplyAdapter: ReReplyAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,6 +41,9 @@ class ViewReplyDetailActivity : BaseActivity() {
 
         getReplyDetailFromServer()
 
+        mReReplyAdapter = ReReplyAdapter(mContext, R.layout.re_reply_list_item, mReReplyList)
+        binding.reReplyListView.adapter = mReReplyAdapter
+
     }
 
     fun getReplyDetailFromServer() {
@@ -54,11 +59,19 @@ class ViewReplyDetailActivity : BaseActivity() {
                     val replyObj = dataObj.getJSONObject("reply")
                     val repliesArr = replyObj.getJSONArray("replies")
 
+                    mReReplyList.clear()
+
                     for (i in 0 until repliesArr.length()) {
 
 //                    [] => {} (JSONObject) 추출 -> ReplyData로 변환 -> 대댓글 목록에 추가 (최종목표)
 
                         mReReplyList.add(ReplyData.getReplyDataFromJson(repliesArr.getJSONObject(i)))
+
+                    }
+
+                    runOnUiThread {
+
+                        mReReplyAdapter.notifyDataSetChanged()
 
                     }
 
