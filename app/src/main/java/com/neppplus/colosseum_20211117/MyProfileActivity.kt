875098c22d548acd1837f190d97093e7477e203a
김준기 <gugi329@daum.net>
2021-11-22 10:21:04 +0900

    로그아웃 처리   - 토큰 삭제 => 로그아웃   - 보이면 모든 화면 종료 -> Splash 화면     => Intent의 flag 기능 활용

--- app/src/main/java/com/neppplus/colosseum_20211117/MyProfileActivity.kt
+++ app/src/main/java/com/neppplus/colosseum_20211117/MyProfileActivity.kt
@@ -1,6 +1,7 @@
 package com.neppplus.colosseum_20211117
 
 import android.content.DialogInterface
+import android.content.Intent
 import android.database.sqlite.SQLiteBindOrColumnIndexOutOfRangeException
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -8,6 +9,7 @@ import androidx.appcompat.app.AlertDialog
 import androidx.databinding.DataBindingUtil
 import com.neppplus.colosseum_20211117.databinding.ActivityMyProfileBinding
 import com.neppplus.colosseum_20211117.databinding.ActivitySplashBinding
+import com.neppplus.colosseum_20211117.utils.ContextUtil
 
 class MyProfileActivity : BaseActivity() {
 
@@ -30,7 +32,15 @@ class MyProfileActivity : BaseActivity() {
             alert.setMessage("정말 로그아웃 하시겠습니까?")
             alert.setPositiveButton("확인", DialogInterface.OnClickListener { dialogInterface, i ->
 
-//                실제 로그아웃 처리
+//                실제 로그아웃 처리 => 저장된 토큰을 ""으로 돌려주자 (내 폰에 저장된 토근 삭제)
+
+                ContextUtil.setToken(mContext, "")
+
+//                화면 종료 -> (열려있는 모든 화면을 전부 닫고) SplashActivity로 이동
+
+                val myIntent = Intent(mContext, SplashActivity::class.java)
+                myIntent.flags = (Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
+                startActivity(myIntent)
 
             })
             alert.setNegativeButton("취소", null)
--- app/src/main/java/com/neppplus/colosseum_20211117/SplashActivity.kt
+++ app/src/main/java/com/neppplus/colosseum_20211117/SplashActivity.kt
@@ -46,6 +46,8 @@ class SplashActivity : BaseActivity() {
 
             startActivity(myIntent)
 
+            finish()
+
         }, 1000)
 
 
