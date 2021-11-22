7f5912e82b64da96369cacd1b082418c2af8d244
김준기 <gugi329@daum.net>
2021-11-22 10:11:58 +0900

    로그아웃 - 경고창 띄우기

--- app/src/main/java/com/neppplus/colosseum_20211117/MyProfileActivity.kt
+++ app/src/main/java/com/neppplus/colosseum_20211117/MyProfileActivity.kt
@@ -1,8 +1,10 @@
 package com.neppplus.colosseum_20211117
 
+import android.content.DialogInterface
 import android.database.sqlite.SQLiteBindOrColumnIndexOutOfRangeException
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import androidx.databinding.DataBindingUtil
 import com.neppplus.colosseum_20211117.databinding.ActivityMyProfileBinding
 import com.neppplus.colosseum_20211117.databinding.ActivitySplashBinding
@@ -19,6 +21,23 @@ class MyProfileActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
+
+        binding.btnLogout.setOnClickListener {
+
+//            진짜 로그아웃할 것인지? 확인
+            val alert = AlertDialog.Builder(mContext)
+            alert.setTitle("로그아웃 확인")
+            alert.setMessage("정말 로그아웃 하시겠습니까?")
+            alert.setPositiveButton("확인", DialogInterface.OnClickListener { dialogInterface, i ->
+
+//                실제 로그아웃 처리
+
+            })
+            alert.setNegativeButton("취소", null)
+
+            alert.show()
+
+        }
 
     }
 
